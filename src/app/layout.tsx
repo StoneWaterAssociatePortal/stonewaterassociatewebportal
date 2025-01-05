9ff8f6149889fe84855e7139442993da345f34b4
StoneWaterAssociatePortal <stonewaterassociateportal@gmail.com>
2025-01-05 08:48:16 -0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import '../app/styles/HomePage.module.css';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,72 @@
+import Image from 'next/image';
+import logo from '../../public/logo.png';  
+import styles from './styles/HomePage.module.css';
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        textAlign: 'center',
+        fontFamily: 'Arial, sans-serif',
+        backgroundColor: '#f9f9f9',
+        padding: '20px',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          marginBottom: '20px',
+          position: 'relative',
+          padding: '16px',
+        }}
+      >
+        <div className={styles.circle} />
+        <Image src={logo} alt="StoneWaterAssociate Logo" width={90} height={60} />
+        <h1
+          style={{
+            marginLeft: '10px',
+            fontSize: '31px',
+            color: '#333',
+            fontWeight: '500',
+            wordBreak: 'break-word',
+          }}
+        >
+          StoneWaterAssociate
+        </h1>
+      </div>
+
+      <h2
+        style={{
+          fontSize: '70px',
+          color: '#555',
+          marginBottom: '20px',
+          fontWeight: '900',
+          wordBreak: 'break-word',
+          marginTop: '-40px',
+        }}
+      >
+        We're Working on <br/> Something Exciting
+      </h2>
+
+      <p
+        style={{
+          fontSize: '18px',
+          color: '#666',
+          maxWidth: '600px',
+          lineHeight: '1.5',
+          fontWeight: '900',
+        }}
+      >
+        The StoneWaterAssociate web portal is currently under construction.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
