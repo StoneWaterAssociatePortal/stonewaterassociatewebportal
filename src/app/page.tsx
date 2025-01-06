ed6ceb577d809c6ff350e56a868e850c7d3497a3
StoneWaterAssociatePortal <stonewaterassociateportal@gmail.com>
2025-01-06 06:31:42 -0800

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,65 +4,17 @@ import styles from './styles/HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        textAlign: 'center',
-        fontFamily: 'Arial, sans-serif',
-        backgroundColor: '#f9f9f9',
-        padding: '21px',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          marginBottom: '20px',
-          position: 'relative',
-          padding: '16px',
-        }}
-      >
-        <div className={styles.circle} />
-        <Image src={logo} alt="StoneWaterAssociate Logo" width={90} height={60} />
-        <h1
-          style={{
-            marginLeft: '10px',
-            fontSize: '32px',
-            color: '#333',
-            fontWeight: '500',
-            wordBreak: 'break-word',
-          }}
-        >
-          StoneWaterAssociate
-        </h1>
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <Image src={logo} alt="StoneWaterAssociate Logo" width={90} height={60} className={styles.logo} />
+        <h1 className={styles.title}>StoneWaterAssociate</h1>
       </div>
 
-      <h2
-        style={{
-          fontSize: '70px',
-          color: '#555',
-          marginBottom: '20px',
-          fontWeight: '900',
-          wordBreak: 'break-word',
-          marginTop: '-40px',
-        }}
-      >
-        We're Working on <br/> Something Exciting
+      <h2 className={styles.subHeader}>
+        We're Working on <br /> Something Exciting
       </h2>
 
-      <p
-        style={{
-          fontSize: '18px',
-          color: '#666',
-          maxWidth: '600px',
-          lineHeight: '1.5',
-          fontWeight: '900',
-        }}
-      >
+      <p className={styles.description}>
         The StoneWaterAssociate web portal is currently under construction.
       </p>
     </div>
